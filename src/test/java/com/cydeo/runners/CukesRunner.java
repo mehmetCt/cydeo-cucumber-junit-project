872127183bc5f1c260a934e7f4adf222e6b05a89
mehmetCt <mehmet@cybertekschool.com>
2023-03-04 15:55:40 -0500

    First

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -9,12 +9,13 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = " @wip",
+        tags = " @smoke",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
